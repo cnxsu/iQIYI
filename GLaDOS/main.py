2a1b0f0bbb7b8554fb3a11da277bd010d11e0087
神经蛙 <121377609+gopcn@users.noreply.github.com>
2024-02-29 23:46:19 +0800

    Add files via upload

--- GLaDOS/main.py
+++ GLaDOS/main.py
@@ -1,11 +1,10 @@
 from os import environ
 from logger import logger
 from Check import CheckIn
-from push import send_msg_ServerChan, send_msg_PushPlus, send_msg_Qmsg
-
+from push import send_msg_ServerChan, send_msg_PushPlus, send_msg_Qmsg, send_msg_WxPusher
 
 def main():
-    # 获取actions secrets配置的cookie SendKey
+    # 从环境变量中获取配置信息
     ck = environ.get("cookie")
     SendKey = environ.get("SendKey")
     is_ServerChan_push = int(environ.get("is_ServerChan_push", 0))
@@ -13,12 +12,16 @@ def main():
     is_PushPlus_push = int(environ.get("is_PushPlus_push", 0))
     key = environ.get("key")
     is_Qmsg_push = int(environ.get("is_Qmsg_push", 0))
+    appToken = environ.get("appToken")
+    uids = environ.get("uid")
 
+    # 检查是否存在cookie配置
     if not ck:
         logger.info("请先配置GLADOS_COOKIE！")
         return
 
     try:
+        # 进行签到操作
         title, msg = CheckIn(ck)
         logger.info("GLaDOS 签到成功！")
     except Exception as err:
@@ -26,20 +29,24 @@ def main():
         title = "程序运行出错！"
         msg = str(err)
     finally:
+        # 按行输出签到信息
         tmp = msg.split("\n")
         for i in tmp:
             logger.info(i)
 
-        if is_ServerChan_push and SendKey:  # server酱推送消息，无SendKey不推送
+        # 根据配置向不同平台推送签到信息
+        if is_ServerChan_push and SendKey:
             rsp1 = send_msg_ServerChan(SendKey, title, msg)
             logger.info(rsp1)
-        if is_PushPlus_push and token:  # pushplus推送消息，无token不推送
+        if is_PushPlus_push and token:
             rsp2 = send_msg_PushPlus(token, title, msg)
             logger.info(rsp2)
-        if is_Qmsg_push and key:  # Qmsg推送消息，无key不推送
+        if is_Qmsg_push and key:
             rsp3 = send_msg_Qmsg(key, msg)
             logger.info(rsp3)
-
+        if appToken and uids:
+            rsp4 = send_msg_WxPusher(appToken, uids, title, msg)
+            logger.info(rsp4)
 
 if __name__ == "__main__":
     main()
--- GLaDOS/push.py
+++ GLaDOS/push.py
@@ -1,45 +1,42 @@
-from json import dumps
-from time import sleep
+import requests
 from logger import logger
-from requests import post, get
-
 
 def send_msg_ServerChan(SendKey, title, msg):
     if not SendKey:
-        return 'Sever酱: 未配置SendKey，无法进行消息推送。'
+        return 'Sever酱: 未配置SendKey，无法进行消息推送.'
 
     logger.info('========================================')
     logger.info('Sever酱: 开始推送消息！')
-    msg = msg.replace('\n', '\n\n')
+
     url = f'https://sctapi.ftqq.com/{SendKey}.send'
     data = {'title': title, 'desp': msg, 'channel': 9}
-    rsp = post(url=url, data=data)
+    rsp = requests.post(url, data=data)
     pushid = rsp.json()['data']['pushid']
     readkey = rsp.json()['data']['readkey']
     state_url = f'https://sctapi.ftqq.com/push?id={pushid}&readkey={readkey}'
 
     count = 1
     while True:
-        status_rsp = get(url=state_url)
+        status_rsp = requests.get(state_url)
         result = status_rsp.json()['data']['wxstatus']
-        logger.info(f'查询消息推送是否成功ing : {count}')
+        logger.info(f'查询消息推送是否成功：{count}')
 
         if result:
             return '消息推送成功！'
         elif count >= 60:   # 防止程序一直运行
             return '程序运行结束！推送结果未知！'
         count += 1
-        sleep(1)
-
+        time.sleep(1)
 
 def send_msg_PushPlus(token, title, msg):
     if not token:
-        return 'PushPlus: 未配置token，无法进行消息推送。'
+        return 'PushPlus: 未配置token，无法进行消息推送.'
 
     logger.info('========================================')
     logger.info('PushPlus: 开始推送消息！')
+
     url = 'http://www.pushplus.plus/send/'
-    headers = {'Content-Type':'application/json'}
+    headers = {'Content-Type': 'application/json'}
     data = {
         "token": token,
         "title": title,
@@ -47,19 +44,49 @@ def send_msg_PushPlus(token, title, msg):
         "template": "txt",
         "channel": "wechat"
     }
-    rsp = post(url=url, json=data, headers=headers)
+    rsp = requests.post(url=url, json=data, headers=headers)
     return rsp.json()['msg']
 
+def send_msg_WxPusher(appToken, uids, title, msg):
+    if not (appToken and uids):
+        return 'WxPusher: 未配置appToken和uids，无法进行消息推送.'
+
+    logger.info('========================================')
+    logger.info('WxPusher: 开始推送消息！')
+
+    url = 'https://wxpusher.zjiecode.com/api/send/message'
+    headers = {'Content-Type': 'application/json'}
+
+    data = {
+        "appToken": appToken,
+        "content": msg,
+        "summary": title,
+        "contentType": 1,
+        "uids": [uids]
+    }
+
+    try:
+        response = requests.post(url, json=data, headers=headers)
+        response_data = response.json()
+
+        if response_data.get('code') == 1000:
+            return 'WxPusher: 消息推送成功！'
+        else:
+            return f'WxPusher: 消息推送失败 - {response_data.get("msg")}'
+    except Exception as e:
+        return f'WxPusher: 消息推送失败 - {str(e)}'
 
 def send_msg_Qmsg(key, msg):
     if not key:
-        return 'Qmsg: 未配置key，无法进行消息推送。'
+        return 'Qmsg: 未配置key，无法进行消息推送.'
 
     logger.info('========================================')
     logger.info('Qmsg: 开始推送消息！')
+
     url = f'https://qmsg.zendee.cn:443/send/{key}'
     params = {'msg': msg}
-    rsp = get(url=url, params=params).json()
+    rsp = requests.get(url=url, params=params).json()
+
     if rsp and rsp['success']:
-        return '消息推送成功！'
-    return rsp
+        return 'Qmsg: 消息推送成功！'
+    return 'Qmsg: 消息推送失败！'
